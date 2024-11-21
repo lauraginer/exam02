743b9768fd93f90259e3eb55cf97716e8c63dc57
Laura Maria Giner Mata <lginer-m@c3r8s4.42malaga.com>
2024-11-21 22:14:22 +0100

    viene lo chungo guapa

---
+++ rev_print.c
@@ -0,0 +1,28 @@
+//#include <unistd.h>
+
+char *rev_print(char *str)
+{
+	int i;
+	int len;
+
+	len = 0;
+	while(str[len])
+		len++;
+	i = len - 1; //situarlo antes del caracter nulo
+	while(str[i] != 0)
+	{
+		write(1, &str[i], 1);
+		i--;
+	}
+	return(str);
+}
+
+/*int main()
+{
+  rev_print("Hello world");
+  write (1, "\n", 1);
+  rev_print("tnirp esreveR");
+  write (1, "\n", 1);
+  rev_print("");
+  write (1, "\n", 1);
+}*/
