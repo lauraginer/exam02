99f457e7d4a4d53c9c328558c6ad75ea25f0b893
Laura Maria Giner Mata <lginer-m@c1r7s4.42malaga.com>
2024-11-27 22:58:36 +0100

    level 1 completo

--- search_and_replace.c
+++ search_and_replace.c
@@ -1,29 +1,24 @@
 #include <unistd.h>
 
+void search_replace(char letter, char vocal, char replace)
+{
+	if(letter == vocal)
+		letter = replace;
+	write(1, &letter, 1);
+}
 int main (int argc, char **argv)
 {
 	int i;
-
 	i = 0;
-	if(argc == 4)
+	if (argc == 4 && argv[2][0] && argv[3][0] && !argv[2][1] && !argv[3][1])
 	{
-		write(1, "hola", 4);
-	}
-	else if (!argv[1][i]) //corregir, si el segundo argv no existe en la cadena haz esto
-	{
-		write(1, &argv[1][i], 1);
-		write(1, "\n", 1);
+		while(argv[1][i])
+		{
+			search_replace(argv[1][i], argv[2][0], argv[3][0]);
+			i++;
+		}
 	}
 	else
 		write(1, "\n", 1);
 	return(0);
-}
-
-void search(char letter)
-{
-
-}
-void replace(char letter)
-{
-	
 }
