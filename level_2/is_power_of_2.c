01bfe61f5e1841751a45f5ebc077563101f15b69
Laura Maria Giner Mata <lginer-m@c1r3s4.42malaga.com>
2024-12-04 22:43:05 +0100

    espabilacoñostiaputa

--- level_2/is_power_of_2.c
+++ level_2/is_power_of_2.c
@@ -1,5 +1,24 @@
-int	    is_power_of_2(unsigned int n)
-{
+//#include <stdio.h>
 
-	
+int is_power_of_2(unsigned int n)
+{
+	if(n == 0)
+		return(0);
+	while(n % 2 == 0)
+		n = n / 2;
+	if(n == 1)
+		return(1);
+	else
+		return(0);
 }
+
+/*int main ()
+{
+	int i;
+	int result;
+
+	i = 2;
+	result = is_power_of_2(i);
+	printf("valor de la funcion: %d\n", result);
+	return(0);
+}*/
---
+++ level_2/max.c
@@ -0,0 +1,29 @@
+//#include <stdio.h>
+
+int		max(int* tab, unsigned int len)
+{
+	int result;
+	int i = 0;
+	
+	if (len == 0)
+		return (0);
+	result = tab[i];
+	while(i < len)
+	{
+		if(result < tab[i])
+			result = tab[i];
+		i++;
+	}
+	return result;
+}
+/*int main()
+{
+
+	int tab[] = {1, 5, 3, 4, 7, 30};
+	unsigned int len = sizeof(tab) / sizeof(tab[0]);
+	int result;
+
+	result = max(tab, len);
+	printf("valor de la funcion: %d\n", result);
+	return(0);
+}*/
