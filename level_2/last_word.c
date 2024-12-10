e7c99c93f00366b2a6ebe6750be4f1d7e038e49f
Laura Maria Giner Mata <lginer-m@c1r11s6.42malaga.com>
2024-12-10 20:25:33 +0100

    animo porque lo necesitas

--- level_2/last_word.c
+++ level_2/last_word.c
@@ -1,35 +1,27 @@
 #include <unistd.h>
 
-char last_word(char *str)
+void last_word(char *str)
 {
-	int i;
+	int j = 0;
+	int i = 0;
 
-	i = 0;
-	while(str[i])
-		i++;
-	while(str[i] > 0)
+	while (str[i])
 	{
-		if(str[i] == " " || str[i] == "\t")
-			return(str);
+		if (str[i] == ' ' && str[i + 1] >= ' ' && str[i + 1] <= 126)
+			j = i + 1;
 		i++;
 	}
-	return(*str);
-	write(1, &str, 1);
+	while (str[j] >= ' ' && str[j] <= 127)
+	{
+		write(1, &str[j], 1);
+		j++;
+	}
 }
 
 int main (int argc, char **argv)
-{ 
-	int i;
-
-	i = 0;
+{
 	if(argc == 2)
-	{
-		while(argv[1][i])
-		{
-			last_word(argv[1]);
-		}
-	write(1, "\n", 1);
-	}
+		last_word(argv[1]);
 	else
 		write(1, "\n", 1);
 	return(0);
