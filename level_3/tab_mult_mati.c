85a34a6e91f561c40f17a10f4210fbdefaa73c4b
Laura Maria Giner Mata <lginer-m@c2r5s4.42malaga.com>
2025-02-06 21:27:15 +0100

    queda poco

---
+++ level_3/tab_mult_mati.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int ft_atoi(char *str)
+{
+	int i = 0;
+	int num;
+	num = 0;
+	while(str[i])
+	{
+		num = num * 10 + str[i] - '0';
+		i++;
+	}
+	return(num);
+}
+
+void print_n(int n)
+{
+	char *str = "0123456789";
+
+	if(n > 9)
+		print_n(n / 10);
+	write(1, &str[n % 10], 1);
+}
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	if(argc == 2)
+	{
+		while(i <= 9)
+		{
+			print_n(i);
+			write(1," X ",3);
+			print_n(ft_atoi(argv[1]));
+			write(1," = ",3);
+			print_n(ft_atoi(argv[1]) * i);
+			write(1,"\n", 1);
+			i++;
+		}
+	}
+	else
+		write(1, "\n", 1);
+}
